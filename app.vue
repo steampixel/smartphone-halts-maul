2db00eb8c067f9287a40ab62434e0f0778cfe1e3
Steampixel <info@steampixel.de>
2023-10-25 21:23:24 +0200

    Added github info

--- app.vue
+++ app.vue
@@ -150,6 +150,8 @@
         Ein <a target="_blank" href="https://steampixel.de">steampixel</a> Projekt | 
         Version: {{ version }} | 
         Stand: {{ lastUpdated }} | 
+        Lizenz: MIT | 
+        <a target="_blank" href="https://github.com/steampixel/smartphone-halts-maul">GitHub</a>
         <a target="_blank" href="https://steampixel.de/impressum/">Impressum</a> |
         <a target="_blank" href="https://steampixel.de/datenschutzerklaerung/">Datenschutz</a>
       </p>
