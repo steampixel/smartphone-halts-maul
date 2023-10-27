1f56c6c20d8cb2c6f8f21524a481c77ff6e1ee46
Steampixel <info@steampixel.de>
2023-10-27 08:49:58 +0200

    Added flyer page

---
+++ pages/flyer.vue
@@ -0,0 +1,30 @@
+<template>
+       
+          
+
+    <div class="container">
+
+        <space></space>
+        <space></space>
+        <space></space>
+        <space></space>
+
+        <h1 class="text-gray-800">
+            <div class="text-5xl md:text-6xl font-bold mb-2">🦄 smartphone-halts-maul.de</div>
+            <div class="text-2xl">✔ Eine Checkliste für deine mobile Sicherheit</div>
+        </h1>
+
+        <space></space>
+        <space></space>
+
+    </div>
+
+       
+</template>
+  
+<script setup>
+
+    // import AppButton from '~/components/Button.vue'
+
+</script>
+
